06e1f0fa5f33c64d1dddb173c8a6a7946ddac5c4
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-20 03:23:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+  
+}
 
+function nestedTarget(){
+ return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < rankedLists.length; i++){
+    rankedLists[i].innerHTML = (parseInt(rankedLists[i].innerHTML)+3).toString()
+  }
+}
+
+function deepestChild(){
+   var list = document.querySelectorAll('#grand-node');
+   while (typeof list.children[0] != 'undefined'){
+      list = list.children[0];
+   }
+   return list
+   
+}
